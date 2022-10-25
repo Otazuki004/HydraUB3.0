65e09c8f689ddf19036a914509450317c83dbb18
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2022-10-25 08:03:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+LOG_GROUP_ID = -1001681859325
+OWNER_ID = [1985665341]
+HANDLER = ["~", ".", "!"]
+ALIVE_TEXT = """ **~  Hydra System:**
+━━━━━━━━━━━━━━━━━━━
+
+❥ **𝑂𝑊𝑁𝐸𝑅**: {}
+❥ **𝑉𝐸𝑅𝑆𝐼𝑂𝑁:** {}
+❥ **𝑃𝑂𝑁𝐺 𝑀𝑠:** {}
+❥ **𝑈𝑃𝑇𝐼𝑀𝐸:** {}
+
+━━━━━━━━━━━━━━━━━━━
+**Meet Me Here🙈 @wesupport004 ✨🥀**
+"""
+
+NEKO = "https://te.legra.ph/file/e5a51885a947f7b8232af.jpg"
 
